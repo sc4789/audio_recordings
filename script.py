5456a417e7002b5f85419918970d91e543c7ad06
sc4789 <sc4789@columbia.edu>
2024-04-04 03:50:03 +0000

    script for extracting files from repo

---
+++ script.py
@@ -0,0 +1,38 @@
+import os
+import json
+from subprocess import Popen, PIPE
+
+def convert_to_flac(m4a_file):
+    flac_file = m4a_file[:-3] + "flac"
+    command = f"ffmpeg -i {m4a_file} {flac_file}"
+    process = Popen(command.split(), stdout=PIPE, stderr=PIPE)
+    output, error = process.communicate()
+    return flac_file if os.path.exists(flac_file) else flac_file
+
+def recognize_audio(file_path):
+    command = f"gcloud ml speech recognize {file_path} --language-code=en-US"
+    process = Popen(command.split(), stdout=PIPE, stderr=PIPE)
+    output, error = process.communicate()
+    return output.decode("utf-8")
+
+def main():
+    output_file = "output.txt"
+    folder_path = "test"
+    
+    with open(output_file, "w") as f_out:
+        for filename in os.listdir(folder_path):
+            if filename.endswith(".m4a"):
+                audio_file = os.path.join(folder_path, filename)
+                flac_file = convert_to_flac(audio_file)
+                recognition_result = recognize_audio(flac_file)
+                f_out.write(f"File: {filename}\n")
+                try:
+                    json_output = json.loads(recognition_result)
+                    f_out.write(json.dumps(json_output, indent=4))
+                except json.JSONDecodeError:
+                    f_out.write("Error: Unable to decode JSON output")
+                f_out.write("\n\n")
+                os.remove(flac_file)  # Remove the temporary FLAC file
+
+if __name__ == "__main__":
+    main()
